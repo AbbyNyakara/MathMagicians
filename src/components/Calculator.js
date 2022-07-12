432a8c2ebf160c4df0545268c69ce832d96985d1
Abby Nyakara <abbynyakara@gmail.com>
2022-07-12 16:03:33 +0300

    create multiple pages using react router

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import Header from './Header';
 
 function Calculator () {
   const [state, setState] = useState({ total: 0, next: 0 });
@@ -15,27 +16,33 @@ function Calculator () {
   };
 
   return (
-    <div className="grid">
-      <button className="top-bar" onChange={handleDisplay}>{state.next || state.total || 0}</button>
-      <button type="button" value="AC" onClick={handleClick}>AC</button>
-      <button type="button" value="+/-" onClick={handleClick}>+/-</button>
-      <button type="button" value="%" onClick={handleClick}>%</button>
-      <button type="button" value="÷" className="orange" onClick={handleClick}>÷</button>
-      <button type="button" value="7" onClick={handleClick}>7</button>
-      <button type="button" value="8" onClick={handleClick}>8</button>
-      <button type="button" value="9" onClick={handleClick}>9</button>
-      <button type="button" value="x" className="orange" onClick={handleClick}>x</button>
-      <button type="button" value="4" onClick={handleClick}>4</button>
-      <button type="button" value="5" onClick={handleClick}>5</button>
-      <button type="button" value="6" onClick={handleClick}>6</button>
-      <button type="button" value="-" className="orange" onClick={handleClick}>-</button>
-      <button type="button" value="1" onClick={handleClick}>1</button>
-      <button type="button" value="2" onClick={handleClick}>2</button>
-      <button type="button" value="3" onClick={handleClick}>3</button>
-      <button type="button" value="+" className="orange" onClick={handleClick}>+</button>
-      <button type="button" value="0" className="bottom-bar" onClick={handleClick}>0</button>
-      <button type="button" value="." onClick={handleClick}>.</button>
-      <button type="button" value="=" className="orange" onClick={handleClick}>=</button>
+    <div>
+      <Header />
+      <div className='calc-container'>
+        <h1>Let's do some math!</h1>
+        <div className="grid">
+          <button className="top-bar" onChange={handleDisplay}>{state.next || state.total || 0}</button>
+          <button type="button" value="AC" onClick={handleClick}>AC</button>
+          <button type="button" value="+/-" onClick={handleClick}>+/-</button>
+          <button type="button" value="%" onClick={handleClick}>%</button>
+          <button type="button" value="÷" className="orange" onClick={handleClick}>÷</button>
+          <button type="button" value="7" onClick={handleClick}>7</button>
+          <button type="button" value="8" onClick={handleClick}>8</button>
+          <button type="button" value="9" onClick={handleClick}>9</button>
+          <button type="button" value="x" className="orange" onClick={handleClick}>x</button>
+          <button type="button" value="4" onClick={handleClick}>4</button>
+          <button type="button" value="5" onClick={handleClick}>5</button>
+          <button type="button" value="6" onClick={handleClick}>6</button>
+          <button type="button" value="-" className="orange" onClick={handleClick}>-</button>
+          <button type="button" value="1" onClick={handleClick}>1</button>
+          <button type="button" value="2" onClick={handleClick}>2</button>
+          <button type="button" value="3" onClick={handleClick}>3</button>
+          <button type="button" value="+" className="orange" onClick={handleClick}>+</button>
+          <button type="button" value="0" className="bottom-bar" onClick={handleClick}>0</button>
+          <button type="button" value="." onClick={handleClick}>.</button>
+          <button type="button" value="=" className="orange" onClick={handleClick}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+import React from 'react';
+import Header from './Header';
+
+const Quote = () => {
+  return(
+    <div>
+      <Header />
+      <h2 className='quote'>Mathematics is not abour numbers, equations and computations or algorithms. 
+      It's about understanding - William Paul Thurston</h2>
+    </div>
+  )
+}
+
+export default Quote;
